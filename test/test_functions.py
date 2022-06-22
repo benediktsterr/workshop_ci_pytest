4a0631eca29ca0323966e24fa8729bf9722f9b45
JulianKarlBauer <55383008+JulianKarlBauer@users.noreply.github.com>
2022-06-22 11:29:34 +0200

    Try printing

--- test/test_functions.py
+++ test/test_functions.py
@@ -1,4 +1,10 @@
 from functions import add
+import numpy as np
 
 def test_add():
   assert add(3, 4) == 7
+  
+def test_show_printing():
+  matrix = np.random.rand(5,5)
+  print(matrix)
+  assert False
